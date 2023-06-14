36fa269427810e13b37d5285d7f99103158d7dd4
haroldsollorano <haroldsollorano@awesomelabph.com>
2023-06-14 18:05:56 +0800

    test

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 var serveIndex = require('serve-index')
 const app = express()
 const port = 3100
-app.use('/ftp', express.static('/uploads'), serveIndex('uploads', {'icons': true, 'view': 'details'}))
+
+app.use('/ftp', express.static('uploads'), serveIndex('uploads', {'icons': true, 'view': 'details'}))
 
 
 app.use('/',require('./routes/controller'))
